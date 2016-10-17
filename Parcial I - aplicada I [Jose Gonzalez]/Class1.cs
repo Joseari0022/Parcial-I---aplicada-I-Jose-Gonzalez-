f855d020c75093c85afea878787e332e4e357400
toshiba <toshiba@DESKTOP-CI7DQBE>
2016-10-17 18:23:03 -0400

    Add project files.

---
+++ Parcial I - aplicada I [Jose Gonzalez]/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Parcial_I___aplicada_I__Jose_Gonzalez_
+{
+    public class Class1
+    {
+    }
+}
